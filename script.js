9b7029f341cbb64c619ceda4d4d091c120175d1f
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-10-01 17:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // write js code here if required
+function timer(){
+    const now = new Date()
+
+
+    let day = String(now.getDate()).padStart(2,`0`)
+    let month = String(now.getMonth()+1).padStart(2,`0`)
+    let year = now.getFullYear()
+    let second = String(now.getSeconds()).padStart(2,`0`)
+    let minute = String(now.getMinutes()).padStart(2,`0`)
+    let hour = String(now.getHours()).padStart(2,`0`)
+
+    let display = `${day}:${month}:${year}, ${hour}/${minute}/${second}`
+
+    document.querySelector("#timer").innerText = display
+}
+setInterval(timer,1000)
+timer()
